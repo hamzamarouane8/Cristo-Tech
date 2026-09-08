5bbbdf3d7768d99520178b3a1ad01f470944ffb6
Marouane hamza <46194912+hamzamarouane8@users.noreply.github.com>
2026-09-08 10:41:08 +0100

    Update xxx.avtice.ts

--- xxx.avtice.ts
+++ xxx.avtice.ts
@@ -1,3 +1,4 @@
-npm i @angular/animations@17.3.12 @angular/common@17.3.12 @angular/compiler@17.3.12 @angular/core@17.3.12 @angular/forms@17.3.12 @angular/platform-browser@17.3.12 @angular/platform-browser-dynamic@17.3.12 @angular/router@17.3.12 @angular/cdk@17.3.10 zone.js@0.14.10 --legacy-peer-deps
-
-npm i -D @angular/cli@17.3.17 @angular/compiler-cli@17.3.12 @angular-devkit/build-angular@17.3.17 typescript@5.4.5 ngx-build-plus@17.0.0 @angular-architects/module-federation@17.0.8 --legacy-peer-deps
+rmdir /s /q node_modules
+del package-lock.json
+npm i
+npm ls @angular/core @angular-devkit/build-angular
