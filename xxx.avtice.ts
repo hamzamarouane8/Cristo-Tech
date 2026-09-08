c980de55d0cd4509b62185bfbb8c41d1d21a273d
Marouane hamza <46194912+hamzamarouane8@users.noreply.github.com>
2026-09-08 11:25:00 +0100

    Update xxx.avtice.ts

--- xxx.avtice.ts
+++ xxx.avtice.ts
@@ -1,4 +1,11 @@
-rmdir /s /q node_modules
-del package-lock.json
-npm i
-npm ls @angular/core @angular-devkit/build-angular
+"styles": [
+  "src/styles.scss",
+  "node_modules/@angular/material/prebuilt-themes/indigo-pink.css",
+  "node_modules/quill/dist/quill.snow.css"
+]
+
+npm i @angular/material@17
+
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+shared: shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
